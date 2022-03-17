53111b060efce37529648258591e917ebeed2fc6
Athul-Krishna <athulkrishnaks1998@gmail.com>
2022-03-18 00:32:58 +0530

    Add firebase sdk support

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp();
     return Scaffold(
       body: ListView.builder(
         itemCount: 10,
@@ -12,6 +15,19 @@ class ChatScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           child: const Text('This works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/2fq17TMvHjy2OVN171Dv/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((doc) {
+              print(doc['text']);
+            });
+          });
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
